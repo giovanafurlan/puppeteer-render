808a19ff03a56605b079068de60b07b8619f6a20
giovanaFurlan <giovananelofurlan@gmail.com>
2024-03-12 11:15:31 -0300

    fix: Nova página

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
-    headless: false,
+    // headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -17,79 +17,81 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://www.linkedin.com");
+    await page.goto("https://www.linkedin.com/in/joao-pedro-eb/");
+
+    await page.waitForTimeout(5000);
+
+    const screenshot = await page.screenshot({ path: 'screenshot.png' });
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
-
-    const screenshot = await page.screenshot("/screenshot.png");
 
     let sobre;
     let funcao;
     let localizacao;
     let experiencias;
 
-    // // pegar título
-    // const title = await page.title();
+    // pegar título
+    const title = await page.title();
 
-    // // pegar conteúdo sobre
-    // sobre = await page.evaluate(() => {
-    //   const span = document.querySelector(".core-section-container__content");
-    //   if (span) {
-    //     return span.textContent.trim();
-    //   }
-    //   return null;
-    // });
+    // pegar conteúdo sobre
+    sobre = await page.evaluate(() => {
+      const span = document.querySelector(".core-section-container__content");
+      if (span) {
+        return span.textContent.trim();
+      }
+      return null;
+    });
 
-    // // pegar conteúdo função
-    // funcao = await page.evaluate(() => {
-    //   const span = document.querySelector(".top-card-layout__headline");
-    //   if (span) {
-    //     return span.textContent.trim();
-    //   }
-    //   return null;
-    // });
+    // pegar conteúdo função
+    funcao = await page.evaluate(() => {
+      const span = document.querySelector(".top-card-layout__headline");
+      if (span) {
+        return span.textContent.trim();
+      }
+      return null;
+    });
 
-    // // pegar conteúdo localização
-    // localizacao = await page.evaluate(() => {
-    //   const span = document.querySelector(
-    //     "div.not-first-middot span:first-child"
-    //   );
-    //   if (span) {
-    //     return span.textContent.trim();
-    //   }
-    //   return null;
-    // });
+    // pegar conteúdo localização
+    localizacao = await page.evaluate(() => {
+      const span = document.querySelector(
+        "div.not-first-middot span:first-child"
+      );
+      if (span) {
+        return span.textContent.trim();
+      }
+      return null;
+    });
 
-    // // pegar conteúdo experiências
-    // experiencias = await page.evaluate(() => {
-    //   const experienceItems = document.querySelectorAll(
-    //     'section[data-section="experience"] .experience-item'
-    //   );
-    //   const experiencesArray = [];
+    // pegar conteúdo experiências
+    experiencias = await page.evaluate(() => {
+      const experienceItems = document.querySelectorAll(
+        'section[data-section="experience"] .experience-item'
+      );
+      const experiencesArray = [];
 
-    //   experienceItems.forEach((item) => {
-    //     const empresa = item
-    //       .querySelector(".experience-item__subtitle")
-    //       .textContent.trim();
-    //     const duracao = item.querySelector(".date-range").textContent.trim();
-    //     const localizacao = item
-    //       .querySelectorAll(".experience-item__meta-item")[1]
-    //       .textContent.trim();
-    //     const descricao = item
-    //       .querySelector(".show-more-less-text__text--less")
-    //       .textContent.trim();
+      experienceItems.forEach((item) => {
+        const empresa = item
+          .querySelector(".experience-item__subtitle")
+          .textContent.trim();
+        const duracao = item.querySelector(".date-range").textContent.trim();
+        const localizacao = item
+          .querySelectorAll(".experience-item__meta-item")[1]
+          .textContent.trim();
+        const descricao = item
+          .querySelector(".show-more-less-text__text--less")
+          .textContent.trim();
 
-    //     experiencesArray.push({
-    //       empresa,
-    //       duracao,
-    //       localizacao,
-    //       descricao,
-    //     });
-    //   });
+        experiencesArray.push({
+          empresa,
+          duracao,
+          localizacao,
+          descricao,
+        });
+      });
 
-    //   return experiencesArray;
-    // });
+      return experiencesArray;
+    });
 
     // Retornando a resposta como JSON
     // res.json({ title, sobre, funcao, localizacao, experiencias });
